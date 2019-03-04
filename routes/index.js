9f5dd2e5f94c996436850991b1e5b546d0b91c62
kittyyuhong <929508586@qq.com>
2019-03-04 21:41:04 +0800

    Merge branch 'master' of github.com:kittyyuhong/test0304
    # Conflicts:
    #	views/myTestView.ejs

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 /*test K github进dgsgsg22222行测试*/
+/*dggssdgsdg*/
 module.exports = router;
 
 router.get('/myTest', function(req, res, next) {
