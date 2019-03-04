3af7269d40ee334754762cdfb4550b361077eead
kittyyuhong <929508586@qq.com>
2019-03-04 20:44:29 +0800

    第一次github提交测试

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-/*test K*/
+/*test K github进行测试*/
 module.exports = router;
+
+router.get('/myTest', function(req, res, next) {
+    res.render('myTestView', { title: 'This is my Test html!' });
+});
